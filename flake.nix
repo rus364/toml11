cefdf6eed812f40cb6d8ba7cadd490d741b8cce8
Alexander <rus364@gmail.com>
2020-08-28 10:21:07 +0300

    Added flakes.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "toml11 3.5.0";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+
+      stdenv.mkDerivation {
+        pname = "toml11";
+        version = "3.5.0";
+        src = self;
+
+        nativeBuildInputs = [ cmake ];
+        buildInputs = [ boost172 ];
+
+        cmakeFlags = [
+          "-DCMAKE_INSTALL_PREFIX:PATH=$out"
+          "-Dtoml11_BUILD_TEST:BOOL=OFF"
+        ];
+
+      };
+
+  };
+
+}
